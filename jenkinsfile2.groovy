a4ce6f47a5f42cade0789b016eb275695e1a1de4
Rakesh Kumar <rakesh0079@gamil.com.com>
2023-02-05 18:43:27 +0530

    pipeline jenkins

---
+++ jenkinsfile2.groovy
@@ -0,0 +1,19 @@
+
+  freeStyleJob('CamundaDSLJOB') {
+
+    description ("First Jenkins job DSL")
+    logRotator(-1, 10)
+    jdk('Java 11')
+    scm {
+        github('rakesh0079/s_camunda.git', 'master')
+    }
+    triggers {
+        scm('*****')
+    }
+    steps {
+        maven('clean package',"pom.xml")
+    }
+    publishers {
+        archiveArtifacts '**/*.war'
+    }
+}
